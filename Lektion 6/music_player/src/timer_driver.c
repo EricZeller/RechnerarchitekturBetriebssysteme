44f4cc3080b259e7c7beeed5149bbf376c3edfe2
EricZeller <98104805+EricZeller@users.noreply.github.com>
2025-05-23 11:50:57 +0200

    lektion 6 fix

--- Lektion 6/music_player/src/timer_driver.c
+++ Lektion 6/music_player/src/timer_driver.c
@@ -22,17 +22,18 @@ extern uint16_t note_freqs[];
 void start_melody(uint16_t ms_per_tick)
 {
     /* Timer0 konfigurieren für 1ms Interrupt */
-    TCCR0A = 0; // Normal mode
+    TCCR0A = 0;                         // Normal mode
     TCCR0B = (1 << CS01) | (1 << CS00); // Prescaler 64 (bei 16MHz: 64/16MHz = 4µs pro Tick)
     TCNT0 = 0;
-    
+
     do
     {
         TCNT0 = 0;
         for (uint16_t ms = 0; ms < ms_per_tick; ms++)
         {
             /* Mit Timer0 1ms warten */
-            while((TIFR0 & (1 << TOV0)) == 0); // Warte auf Overflow
+            while ((TIFR0 & (1 << TOV0)) == 0)
+                ;                 // Warte auf Overflow
             TIFR0 |= (1 << TOV0); // Clear Overflow Flag
         }
     } while (player_tick() != 0);
@@ -48,12 +49,14 @@ void start_melody(uint16_t ms_per_tick)
  */
 void note_on(uint8_t note_number)
 {
-    /* TODO Timer1 für Tonerzeugung konfigurieren*/
+
+    /* Timer1 für Tonerzeugung konfigurieren */
     TCCR1A = (0 << COM1A1) | (1 << COM1A0)           /* Toggle OC1A on Compare Match */
              | (0 << WGM11) | (0 << WGM10);          /* Lower part of WGM1x-Bits */
-    TCCR1B = (0 << CS12) | (0 << CS11) | (1 << CS10) /* Prescaler = 1 */
+    TCCR1B = (0 << CS12) | (1 << CS11) | (1 << CS10) /* Prescaler = 1 */
              | (0 << WGM13) | (1 << WGM12);          /* Upper half of WGM1x-Bits */
-    OCR1A = 18175;                                   /* Length of half-period -1*/
+    OCR1A = note_freqs[note_number-1];             /* MIDI Note 1 = Array Index 0 */
+    TCNT1 = 0;
 }
 
 /**
